70339fbce3f0cc38cace8ed920a8beed9da7da6a
gnzchrs <christophergonzaga63@gmail.com>
2022-11-11 00:26:14 +0800

    landing-page

---
+++ js/script.js
@@ -0,0 +1,27 @@
+AOS.init({ delay: 300, once: true });
+
+var i = 0;
+var p = 0;
+var gived = "Hi! I am Christopher.";
+var pos = "A front-end developer.";
+var speed = 100;
+
+var type = (function nm() {
+  if (i < gived.length) {
+    document.getElementById("name").innerHTML += gived.charAt(i);
+    i++;
+    setTimeout(nm, speed);
+  }
+
+  return nm;
+})();
+
+var type = (function ps() {
+  if (p < pos.length) {
+    document.getElementById("position").innerHTML += pos.charAt(p);
+    p++;
+    setTimeout(ps, speed);
+  }
+
+  return ps;
+})();
